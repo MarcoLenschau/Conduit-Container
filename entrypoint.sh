08d47df752fde687c5cb49c86350dfbb4213cec7
MarcoLenschau <contact@marco-lenschau.de>
2026-01-22 16:11:56 +0100

    chore: Improve the dockerfile with variable and switch to prodative python server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-python /app/backend/manage.py migrate
-python /app/backend/manage.py runserver 0.0.0.0:5000
+cd $
+python manage.py migrate
+gunicorn conduit.wsgi:application --bind 0.0.0.0:5000
